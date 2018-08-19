b7959fb062ac9a86ad5ff0ef5a0ff8f11a3bcfb8
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-08-19 20:49:47 -0300

    feat: Add file for naming colors

---
+++ components/lexicon/foundation/Color.js
@@ -0,0 +1,11 @@
+/*
+ *	Main	#272833	Texts/icons, navigation background, borders and dividers.
+ *	Primary	#0B5FFF	Main actions like primary buttons, links, hover and active.
+ *	White	#FFFFFF	Cards background, toolbar background, modals, forms and texts/icons.
+ *	Light	#F1F2F5	Main background of the application, input’s backgrounds and active state in the secondary botton.
+ */
+
+export const MAIN 		= '#272833';
+export const PRIMARY	= '#0B5FFF';
+export const WHITE		= '#FFFFFF';
+export const LIGHT		= '#F1F2F5';
