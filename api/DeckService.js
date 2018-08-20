7f2e1b853b800119c8324ccfd0b3f50725e71994
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-08-20 13:38:05 -0300

    feat: Add Deck service api

---
+++ api/DeckService.js
@@ -0,0 +1,32 @@
+import DB from 'react-native-db-models';
+import uuid from 'uuid/v4';
+
+const cardDB = new DB.create_db('mobile-flashcards:card');
+export function createDeck({name}) {
+	const now = Date.now();
+	return new Promise(resolve =>
+		cardDB.add({
+			name,
+			created: now,
+			id: uuid(),
+			modified: now,
+			image: 'salmon',
+		}, resolve)
+	);
+}
+
+export function getDeck({id}) {
+
+}
+
+export function updateDeck({id}) {
+
+}
+
+export function getDecks() {
+	return new Promise(resolve =>
+		cardDB.get_all(({rows}) =>
+			resolve(Object.getOwnPropertyNames(rows).map(key => rows[key]))
+		)
+	);
+}
