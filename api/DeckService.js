df06ebb6db7c09bc5097ebb690a79475fe57bd1e
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-08-20 17:22:13 -0300

    fix: Name db correctly

--- api/DeckService.js
+++ api/DeckService.js
@@ -1,11 +1,13 @@
 import DB from 'react-native-db-models';
 import uuid from 'uuid/v4';
 
-const cardDB = new DB.create_db('mobile-flashcards:card');
+const deckDB = new DB.create_db('mobile-flashcards:deck');
+
 export function createDeck({name}) {
 	const now = Date.now();
+
 	return new Promise(resolve =>
-		cardDB.add({
+		deckDB.add({
 			name,
 			created: now,
 			id: uuid(),
@@ -25,7 +27,7 @@ export function updateDeck({id}) {
 
 export function getDecks() {
 	return new Promise(resolve =>
-		cardDB.get_all(({rows}) =>
+		deckDB.get_all(({rows}) =>
 			resolve(Object.getOwnPropertyNames(rows).map(key => rows[key]))
 		)
 	);
@@ -33,7 +35,7 @@ export function getDecks() {
 
 export function deleteAll() {
 	return new Promise(resolve =>
-		cardDB.erase_db(data =>
+		deckDB.erase_db(data =>
 			console.log(data) || resolve(data)
 		)
 	);
