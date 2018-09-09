617f3d8aac50c065723c9b27e209dcae07e47f00
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-09-09 12:33:42 -0300

    refactor: Organize each container as a method to improve readability

--- components/lexicon/satellite/Header.js
+++ components/lexicon/satellite/Header.js
@@ -2,11 +2,99 @@ import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import Icons from '../core/Icons';
 
+const iconSize = 18;
+const fillColor = 'white';
+
 export default class Header extends React.Component {
+	LeftContainer = ({leftButtons}) => (
+		<View style={{
+			flexDirection: 'row',
+			flex: 1,
+		}}>
+			{
+				leftButtons && leftButtons.map((button, index) => (
+					<TouchableOpacity key={index} onPress={button.onPress}>
+						<View style={{
+							justifyContent: 'center',
+							alignItems: 'center',
+							height: 32,
+							width: 32,
+							marginRight: 16,
+						}}>
+							<Icons
+								id={button.icon}
+								size={iconSize}
+								fill={fillColor}
+							/>
+						</View>
+					</TouchableOpacity>
+				))
+			}
+		</View>
+	)
+
+	CenterContainer = ({title, subtitle}) => (
+		<View style={{
+			alignItems: 'center',
+			justifyContent: 'center',
+			height: 27,
+			flex: 1,
+		}}>
+			<Text
+				style={{
+					fontSize: 18,
+					color: fillColor,
+					fontWeight: '600',
+				}}
+				numberOfLines={1}
+			>
+				{title}
+			</Text>
+			{
+				subtitle &&
+					<Text
+						style={{
+							fontSize: 14,
+							color: fillColor,
+						}}
+						numberOfLines={1}
+					>
+						{subtitle}
+					</Text>
+			}
+		</View>
+	)
+
+	RightContainer = ({rightButtons}) => (
+		<View style={{
+			flexDirection: 'row',
+			flex: 1,
+			justifyContent: 'flex-end',
+		}}>
+			{
+				rightButtons && rightButtons.map((button, index) => (
+					<TouchableOpacity key={index} onPress={button.onPress}>
+						<View style={{
+							justifyContent: 'center',
+							alignItems: 'center',
+							height: 32,
+							width: 32,
+							flexDirection: 'row',
+							marginLeft: 16,
+						}}>
+							<Icons
+								size={iconSize}
+								fill={fillColor}
+								id={button.icon}
+							/>
+						</View>
+					</TouchableOpacity>
+				))
+			}
+		</View>
+	)
+
 	render() {
-		const {title, subtitle, leftButtons, rightButtons} = this.props;
-		const iconSize = 18;
-		const fillColor = 'white';
 
 		return (
 			<View style={{
@@ -14,94 +102,19 @@ export default class Header extends React.Component {
 				height: 56,
 				backgroundColor: '#272833',
 				alignItems: 'center',
-				//Header container
 			}}>
-				<View style={{
-					flexDirection: 'row',
-					flex: 1,
-					//Left
-				}}>
-					{
-						leftButtons && leftButtons.map((button, index) => (
-							<TouchableOpacity key={index} onPress={button.onPress}>
-								<View style={{
-									justifyContent: 'center',
-									alignItems: 'center',
-									height: 32,
-									width: 32,
-									marginRight: 16,
-								}}>
-									<Icons
-										id={button.icon}
-										size={iconSize}
-										fill={fillColor}
-									/>
-								</View>
-							</TouchableOpacity>
-						))
-					}
+				<this.LeftContainer
+					{...this.props}
+				/>
 
-				</View>
+				<this.CenterContainer
+					{...this.props}
+				/>
 
-				<View style={{
-					alignItems: 'center',
-					justifyContent: 'center',
-					height: 27,
-					flex: 1,
-					//center
-				}}>
-					<Text
-						style={{
-							fontSize: 18,
-							color: fillColor,
-							fontWeight: '600',
-						}}
-						numberOfLines={1}
-					>
-						{title}
-					</Text>
-					{
-						subtitle &&
-							<Text
-								style={{
-									fontSize: 14,
-									color: fillColor,
-								}}
-								numberOfLines={1}
-							>
-								{subtitle}
-							</Text>
-					}
-				</View>
-
-				<View style={{
-					flexDirection: 'row',
-					flex: 1,
-					justifyContent: 'flex-end',
-					//right
-				}}>
-					{
-						rightButtons && rightButtons.map((button, index) => (
-							<TouchableOpacity key={index} onPress={button.onPress}>
-								<View style={{
-									justifyContent: 'center',
-									alignItems: 'center',
-									height: 32,
-									width: 32,
-									flexDirection: 'row',
-									marginLeft: 16,
-								}}>
-									<Icons
-										size={iconSize}
-										fill={fillColor}
-										id={button.icon}
-									/>
-								</View>
-							</TouchableOpacity>
-						))
-					}
+				<this.RightContainer
+					{...this.props}
+				/>
 			</View>
-		</View>
 		);
 	}
 }
