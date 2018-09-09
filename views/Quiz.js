146d57278665170f8ac20c35983f1ef544a6b0b9
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-09-09 13:01:46 -0300

    feat: Add visual improvements to Quiz buttons

--- views/Quiz.js
+++ views/Quiz.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {
 	View,
 	Text,
+	StyleSheet,
 } from 'react-native';
 import Header from '../components/lexicon/satellite/Header';
 import Button from '../components/lexicon/core/Button';
-import { MAIN } from '../components/lexicon/foundation/Color';
-import { DECK_DETAIL } from './StackedViews';
-import { getDeck } from '../api/DeckService';
+import { MAIN, WHITE } from '../components/lexicon/foundation/Color';
 
 const QUESTION = 'question';
 const ANSWER = 'answer';
@@ -57,6 +56,8 @@ export default class Quiz extends React.Component {
 		});
 	}
 
+	isOngoingQuiz = () => (this.state.currentCardIndex < this.state.cards.length)
+
 	render() {
 		const {
 			currentCardState,
@@ -72,107 +73,89 @@ export default class Quiz extends React.Component {
 					title="Quiz"
 					subtitle={Quiz.getRemainingCardsMessage(cards.length - currentCardIndex)}
 				/>
-				{
-					(currentCardIndex < cards.length) ?
-						<React.Fragment>
-							<View style={{
-								alignItems: 'center',
-								justifyContent: 'center',
-								flex: 1,
-							}}>
-								<Text style={{
-									fontSize: 40,
-									fontWeight: '600',
-									textAlign: 'center',
-									color: MAIN,
-								}}>
-									{
+				<React.Fragment>
+					<View style={styles.contentView}>
+						<Text style={styles.mainText}>
+							{
+								this.isOngoingQuiz() ?
 										cards[currentCardIndex][currentCardState]
-									}
-								</Text>
-							</View>
-							<View style={{
-								flexDirection: 'row',
-							}}>
-								{
-									currentCardState === QUESTION &&
+										: this.getScoreMessage()
+							}
+						</Text>
+					</View>
+					<View style={{
+						backgroundColor: WHITE,
+					}}>
+						{
+							!this.isOngoingQuiz() &&
+								<React.Fragment>
 									<Button
 										type="primary"
-										style={{
-											borderRadius: 0,
-										}}
-										onPress={this.showAnswer}
+										style={styles.button}
+										onPress={this.restartQuiz}
 									>
-										<Text>Show answer</Text>
+										<Text>Restart Quiz</Text>
 									</Button>
-								}
-								{
-									currentCardState === ANSWER &&
-									<React.Fragment>
-										<Button
-											type="primary"
-											style={{
-												borderRadius: 0,
-											}}
-											onPress={() => this.markCurrentCard({correct: true})}
-										>
-											<Text>Correct</Text>
-										</Button>
-										<Button
-											type="primary"
-											style={{
-												borderRadius: 0,
-											}}
-											onPress={() => this.markCurrentCard({correct: false})}
-										>
-											<Text>Incorrect</Text>
-										</Button>
-									</React.Fragment>
-								}
-							</View>
-						</React.Fragment>
-					:
-					<React.Fragment>
-							<View style={{
-								alignItems: 'center',
-								justifyContent: 'center',
-								flex: 1,
-							}}>
-								<Text style={{
-									fontSize: 40,
-									fontWeight: '600',
-									textAlign: 'center',
-									color: MAIN,
-								}}>
-									{this.getScoreMessage()}
-								</Text>
-							</View>
-							<View style={{
-								flexDirection: 'row',
-							}}>
-								<Button
-									type="primary"
-									style={{
-										borderRadius: 0,
-									}}
-									onPress={this.restartQuiz}
-								>
-									<Text>Restart Quiz</Text>
-								</Button>
-								<Button
-									type="primary"
-									style={{
-										borderRadius: 0,
-									}}
-									onPress={this.navigateToDeckView}
-								>
-									<Text>Back to Deck</Text>
-								</Button>
-							</View>
-						</React.Fragment>
-				}
+									<Button
+										type="primary"
+										style={styles.button}
+										onPress={this.navigateToDeckView}
+									>
+										<Text>Back to Deck</Text>
+									</Button>
+							</React.Fragment>
+						}
+						{
+							this.isOngoingQuiz() && currentCardState === QUESTION &&
+							<Button
+								type="primary"
+								style={styles.button}
+								onPress={this.showAnswer}
+							>
+								<Text>Show answer</Text>
+							</Button>
+						}
+						{
+							this.isOngoingQuiz() && currentCardState === ANSWER &&
+								<React.Fragment>
+									<Button
+										type="primary"
+										style={styles.button}
+										onPress={() => this.markCurrentCard({correct: true})}
+									>
+										<Text>Correct</Text>
+									</Button>
+									<Button
+										type="primary"
+										style={styles.button}
+										onPress={() => this.markCurrentCard({correct: false})}
+									>
+										<Text>Incorrect</Text>
+									</Button>
+								</React.Fragment>
+						}
+					</View>
+					</React.Fragment>
 			</View>
 
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	button: {
+		borderRadius: 4,
+		margin: 16,
+	},
+	contentView: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		flex: 1,
+	},
+	mainText: {
+		fontSize: 40,
+		fontWeight: '600',
+		textAlign: 'center',
+		color: MAIN,
+	}
+})
